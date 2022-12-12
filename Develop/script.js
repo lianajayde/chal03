ec7fa5f6926c29a5fb5146049a22c4e84ecb274d
lianajayde <lianajaydee@gmail.com>
2022-12-12 14:44:20 -0600

    My first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,50 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var characterSymbols = "!@#$%^&*()_+";
+var characterNumbers = "0123456789";
+
+
+function generatePassword() {
+  console.log("Button works");
+
+  var chars = "";
+  var password = "";
+  var passwordLength = prompt("Please enter a character amount between 8 and 128");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log("It passed the correct length")
+  var character = confirm("Do you want uppercase letters?");
+  var lowcharacter = confirm("Do you want lower case letters?");
+  var symbol = confirm("Do you want symbols?");
+  var numbers = confirm("Do you want numbers?");
+ 
+  if(character === true){
+    chars = upperCase
+  }
+  if (lowcharacter === true){
+    chars = chars + lowerCase
+  }
+  if (symbol === true){
+    chars = chars + characterSymbols
+  }  
+  if (numbers === true){
+    chars = chars + characterNumbers
+  }
+
+  }
+  else {
+    console.log("It does not reach password requirements")
+  }
+
+
+
+  return "Generated password here";
+
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +57,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Password Generator Starter Code
+This code is created to make a password generator. 
+A password generator creates and gives you a random password.
+
+I started by editing the HTML and adding the requirements for the password as a label.
+I added that the password must be a minimun of 8 characters and no more than 128.
+I added that the password must include atleast one uppercase, one number and one special character.
+
+I declared approriate variables needed for my code to respond to.
+
+Then I moved to the javascript file and made some changes to that.
+I first started by making sure my generated button was clickable.
+I then put some code on the javascript file to make sure that the requirements for the password were met and if it does not meet password requirements a message of
+"does not meet password requirements" will show on the screen.
+I created an if else statement for the password requirements.
+
+
+
+Lastly, I made a new repo throught github and made sure my code was viewable and accessible
+through my repo github.
